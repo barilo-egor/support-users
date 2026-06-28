edd73b7fb76c2ac4cdc7934f850c2a81e6c90624
GrewGh <grew.gh@mail.ru>
2026-06-28 12:54:25 +0300

    init

---
+++ src/main/java/tgb/cryptoexchange/supportusers/SupportUsersApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.supportusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgb/cryptoexchange/supportusers/SupportUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.supportusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
